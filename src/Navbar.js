e214a6194821f808de03983bfa38033c3a891d1f
Alexandr <foghelizang@gmail.com>
2021-10-07 18:10:33 +0300

    First commit

---
+++ src/Navbar.js
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                <div className="logo">Home work #1</div>
+                <ul className="nav-links">
+                    <li>
+                        <a>Home</a>
+                    </li>
+
+                    <li>
+                        <a>Product</a>
+                    </li>
+
+                    <li>
+                        <a>Service</a>
+                    </li>
+
+                    <li>
+                        <a>About</a>
+                    </li>
+
+                    <li>
+                        <a>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
